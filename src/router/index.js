af09ffd8450f1dce75b1464ed4d90a20597ae86f
张平 <feitian.zhang@tuya.com>
2021-12-30 18:12:53 +0800

    feat:子vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import ChildVue from '../views/child-vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'childVue',
+    component: ChildVue
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/childVue',
+    name: 'childVue',
+    component: ChildVue
   }
 ]
 
